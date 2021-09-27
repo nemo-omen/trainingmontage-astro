1a9f15ea89509f0fe815039b79ab123669048155
nemo-omen <nemo.omen@gmail.com>
2021-09-26 19:22:10 -0500

    Start new custom series

---
+++ src/pages/challenges/posts.js
@@ -0,0 +1,11 @@
+const CustomPosts = [
+  {
+    root: 'challenges',
+    title: 'Adventures in Data Structures: The Linked List',
+    date: '2021-09-26 11:54:26',
+    tags: ['AdventuresInDataStructures', 'LinkedList'],
+    description: "In the first of a series demonstrating common data-structures in JavaScript, I tackle the Linked List, a simple structure that's good for storing data that needs frequent insertions and deletions."
+  }
+];
+
+export default CustomPosts;
